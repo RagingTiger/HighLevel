bb9f759ea687dc15a3d4f932b0c9053468ff4e70
RagingTiger <yourmom@example.com>
2022-07-05 17:22:00 -0400

    Implemented custom code feature and refactor

--- app.py
+++ app.py
@@ -1,10 +1,33 @@
 from multiprocessing import Queue, Process
 import streamlit as st
-import transformers
 
 # custom libs
 from packages import model_utils
 
+
+def inference_process(queue, **parameters):
+    # now run inference and store result in IPC queue
+    queue.put(model_utils.model_exec(**parameters))
+
+@st.experimental_memo
+def deploy_model(**parameters):
+    # setup process queue
+    proc_queue = Queue()
+
+    # setup process
+    deploy_proc = Process(
+        target=inference_process,
+        args=[proc_queue],
+        kwargs=parameters
+    )
+
+    # now start and wait
+    deploy_proc.start()
+    result = proc_queue.get()
+    deploy_proc.join()
+
+    # return contents of queue
+    return result
 
 # configuring page
 st.set_page_config(
@@ -26,7 +49,7 @@ with st.sidebar:
 
     # create custom model
     st.header('Custom Model')
-    model_repo_id = st.text_input('Repo Id'),
+    model_repo_id = st.text_input('Repo Id')
     model_name = st.text_input('Name', placeholder='(optional)')
     model_size = st.number_input('Size in GB', value=1.0)
 
@@ -38,7 +61,7 @@ with st.sidebar:
         # as long as repo id config it
         if model_repo_id:
             # set name to repo id if it does not exist
-            model_name = model_name if modle_name else model_repo_id
+            model_name = model_name if model_name else model_repo_id
 
             # now update models
             model_utils.constants.DEFAULT_MODELS[model_name] = {
@@ -56,66 +79,14 @@ with st.sidebar:
     st.header('Custom Execution Code')
     use_custom_code = st.checkbox('Use custom code')
 
-    # get custom model code if any
-    if use_custom_code:
-        # get custom code
-        model_exec_code = st.text_area(
-            '',
-            placeholder='# custom model exec code here'
-        )
-
-        # create exec button
-        update_model_exec_code_button = st.button('Update code')
-
-        # check if update needed
-        if update_model_exec_code_button:
-            # cdisplay ustom model_exec func
-            st.write(model_exec_code)
-
     # clear model/data cache
     st.header('Model/Data Cache')
     clear_cache_auto = st.checkbox('Clear cache automatically')
     clear_cache_now_button = st.button('Clear Cache ')
 
     # check clear button
-    if clear_cache_now_button:
+    if clear_cache_auto or clear_cache_now_button:
         deploy_model.clear()
-
-# setting unique model_exec code
-if not use_custom_code:
-    def default_huggingface_pipeline(task, hf_repo_id, input_data):
-        # using defalut huggingface python code
-        pipe = transformers.pipeline(task, model=hf_repo_id)
-
-        # evalute pipe
-        return pipe(input_data)
-
-    # set executing model to default
-    model_exec = default_huggingface_pipeline
-
-def inference_process(queue, **parameters):
-    # now run inference and store result in IPC queue
-    queue.put(model_exec(*parameters))
-
-@st.experimental_memo
-def deploy_model(**parameters):
-    # setup process queue
-    proc_queue = Queue()
-
-    # setup process
-    deploy_proc = Process(
-        target=inference_process,
-        args=(proc_queue, parameters)
-    )
-
-    # now start and wait
-    deploy_proc.start()
-    result = proc_queue.get()
-    deploy_proc.join()
-
-    # return contents of queue
-    return result
-
 
 # set title
 st.title('High Level')
@@ -149,9 +120,60 @@ inference_task = st.selectbox(
 # select model
 model = st.selectbox('Model', model_utils.gen_model_selection())
 
+# get custom model code if any
+if use_custom_code:
+    # get custom code
+    model_exec_code = st.text_area(
+        'Custom Model Execution Code',
+        placeholder='# custom code here'
+    )
+    # create columns
+    col1, col2 = st.columns([1,1])
+
+    # create exec button
+    with col1:
+        update_model_exec_code_button = st.button('Update')
+
+    # revert if custom code present
+    if model_exec_code:
+        with col2:
+            # create revert button
+            revert_to_default = st.button('Load Default')
+
+            # check if reverted
+            if revert_to_default:
+                # set to default
+                model_utils.model_exec = model_utils.default_huggingface_pipeline
+
+                # and clear cache
+                st.info('Clearing deploy_model() cache ...')
+                deploy_model.clear()
+
+                # notify of default
+                st.success('Model execution code reverted to default!')
+
+        # check if update needed
+        if update_model_exec_code_button:
+            # create new model_exec function
+            custom_func = 'def model_exec(**parameters):\n' + \
+                ''.join((f'  {line}' for line in model_exec_code.splitlines(True)))
+
+            # define new code
+            exec(custom_func)
+
+            # now store in module
+            exec('model_utils.model_exec = model_exec')
+
+            # and clear cache
+            st.info('Clearing deploy_model() cache ...')
+            deploy_model.clear()
+
+            # notify code updated successfully
+            st.success('Custom code loaded!')
+
 # get text
-txt = st.text_area(
-    '',
+user_input = st.text_area(
+    'Input Data',
     height=300,
     placeholder='Enter text here ...',
     value=model_utils.constants.EXAMPLE_TEXT if display_example else ''
@@ -164,17 +186,13 @@ if not submit_button:
     st.stop()
 
 # now summarize
-if txt:
+if user_input:
     # start and wait for finish
     with st.spinner('Model executing ...'):
         inference_result = deploy_model(
             task=inference_task,
-            repo_id=model_utils.constants.DEFAULT_MODELS[model]['repo_id'],
-            input_data=txt
+            hf_repo_id=model_utils.constants.DEFAULT_MODELS[model]['repo_id'],
+            input_data=user_input
         )
         st.success('Inference complete!')
         st.write(inference_result)
-
-# clear cache automatically
-if clear_cache_auto:
-    deploy_model.clear()
--- packages/model_utils.py
+++ packages/model_utils.py
@@ -1,3 +1,5 @@
+import transformers
+
 # custom libs
 from . import constants
 from . import hardware
@@ -10,3 +12,14 @@ def gen_model_selection():
     # get compatible model list based on model size and memory size
     return (model for model, info in constants.DEFAULT_MODELS.items() \
             if info['size_gb'] < mem_size)
+
+# setting unique model_exec code
+def default_huggingface_pipeline(task, hf_repo_id, input_data):
+    # using defalut huggingface python code
+    pipe = transformers.pipeline(task, model=hf_repo_id)
+
+    # evalute pipe
+    return pipe(input_data)
+
+# set executing model to default
+model_exec = default_huggingface_pipeline
